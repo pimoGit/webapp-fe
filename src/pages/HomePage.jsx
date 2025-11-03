f1fd32fe1919c92c9f43486a966360fda0c235a7
Simone <info@simoneicardi.com>
2025-11-03 10:44:29 +0100

    aggiungi pages home e dettaglio

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+// creazione componente Header
+const HomePage = () => {
+    return (
+        <>
+            <h1 className="text-primary">Bool Books</h1>
+            <h2><i>The nerdest book community</i></h2>
+            <div className="row row-cols-3 mt-4">
+                {/* Libri qui */}
+                Qui ci andranno i libri
+            </div>
+        </>
+
+    )
+}
+
+export default HomePage
