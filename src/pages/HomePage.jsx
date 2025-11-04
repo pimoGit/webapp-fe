e125785ca493adb827f2c4bfd55cc38620667807
Simone <info@simoneicardi.com>
2025-11-04 10:28:41 +0100

    setup chiamata axios da home

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,29 @@
+// import axios
+import axios from "axios"
+
+// import state e effetc
+import { useState, useEffect } from "react"
+
 // import comp card libro
 import BookCard from "../components/BookCard"
 
 
 // creazione componente Header
 const HomePage = () => {
+
+    // prepariamo var di stato libri
+    const [books, setBooks] = useState();
+
+    // prepariamo funzione per la chiamata axios
+    const fecthBooks = () => {
+        axios.get('http://localhost:3000/api/books')
+            .then(response => { setBooks(response.data) })
+            .catch(error => { console.log(error) })
+    }
+
+    // faccio partire la chiamata a primo montaggio comp
+    useEffect(fecthBooks, []);
+
     return (
         <>
             <h1 className="text-primary">Bool Books</h1>
